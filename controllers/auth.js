4782492160467652e9256c1b3ddb5327ab85dbca
Santiago Vásquez Yacamán <santiagoyacaman@uninorte.edu.co>
2021-05-30 18:23:39 -0500

    feat: register route works

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const User = require('../models/user')
 const bcrypt = require('bcryptjs')
 const defaultConfig = require('../db/default.json');
+const {validateEmail} = require('../utils/index');
 
 exports.login = async (req, res) => {
   try {
@@ -26,6 +27,32 @@ exports.login = async (req, res) => {
         res.status(200).json({ token })
       },
     )
+  } catch (e) {
+    res.status(500).send('Internal Server Error!')
+  }
+}
+
+exports.register = async (req, res) => {
+  try {
+    const { email=email.trim().toLowerCase(), password=password.trim(), passwordRepeat=passwordRepeat.trim() } = req.body
+    // Backend validation just in case
+    if (!validateEmail(email)) {
+      return res.status(400).send('Email is not valid!');
+    }
+    const user = await User.findOne({email});
+    if (user) {
+      return res.status(409).send("User already exists!")
+    }
+    if (password.length<6) {
+      return res.status(400).send("Password should be at least 6 characters long!");
+    }
+    if (password !== passwordRepeat) {
+      return res.status(400).send("Passwords don't match!");
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    await User.create({email, password: hashedPassword})
+    res.send("User created successfully!");
   } catch (e) {
     res.status(500).send('Internal Server Error!')
   }
