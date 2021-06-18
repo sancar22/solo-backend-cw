ace423d4298b81e8adb0087f1313c7c326c6e7c1
CHARLEY CARRIERO <carriero.char@gmail.com>
2021-06-18 19:16:14 -0400

    finished user controller conversion to typescript

--- controllers/user.ts
+++ controllers/user.ts
@@ -1,20 +1,27 @@
+import {Request, Response} from 'express';
 import User from '../models/user';
 
-export const getInfo = async (req, res) => {
+export const getInfo = async (req: Request, res: Response) => {
   try {
-    const userID = req.user.id;
+    const userID = res.locals.user.id;
     const user = await User.findById(userID)
-      .select({ name: 1, email: 1 })
-      .lean();
-    delete user._id;
-    res.status(200).send(user);
+    .select({ name: 1, email: 1 })
+    .lean();
+
+    if (user) {
+      delete user._id;
+      res.status(200).send(user);
+    } else {
+      res.status(404).send('user not found');
+    }
+
   } catch (e) {
     res.status(500).send('Internal Server Error!');
   }
 };
 
 // This will give information in a way that can be read by the tables in the admin page
-export const getAllUsers = async (req, res) => {
+export const getAllUsers = async (req: Request, res: Response) => {
   try {
     const users = await User.find();
     if (!users) return res.send('No users are available!');
@@ -45,7 +52,7 @@ export const getAllUsers = async (req, res) => {
   }
 };
 
-export const getUserById = async (req, res) => {
+export const getUserById = async (req: Request, res: Response) => {
   try {
     const user = await User.findById(req.params.id);
     if (!user) return res.send('User does not exist!');
