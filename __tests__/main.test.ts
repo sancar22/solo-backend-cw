5adc10374d19eefc05151bc21f2fa473c8d0c1a7
Kip Riecken <kip.riecken@gmail.com>
2021-06-19 16:02:11 -0400

    work on setting up tests for auth controller

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -21,11 +21,85 @@ beforeAll(async () => {
   server = bootServer(port);
 });
 
+describe('POST /auth/login', () => {
+  let endpoint: Test;
+
+  beforeEach(() => {
+    endpoint = request(server).post('/auth/login');
+  });
+
+  // TODO
+  // if user not in mocks, return 401
+  // if invalid username, return 401
+  // if invalid password, return 401
+  // if user account not verified, return 401
+  // if successful, return 200
+
+  test('rejects if user not in mocks', async () => {
+    const response = await endpoint.send({
+      name: 'Bob',
+      email: 'bob@example.com',
+      password: 'password123',
+      admin: false,
+    });
+    expect(response.status).toBe(401);
+  });
+
+  test('rejects if username not valid', async () => {
+    const response = await endpoint.send({
+      name: 'Bob',
+      email: 'bob@example.com',
+      password: 'password123',
+      admin: false,
+    });
+    expect(response.status).toBe(401);
+  });
+
+  test('rejects if password not valid', async () => {
+    const response = await endpoint.send({
+      name: 'Bob',
+      email: 'bob@example.com',
+      password: 'password123',
+      admin: false,
+    });
+    expect(response.status).toBe(401);
+  });
+
+  test('rejects if user account not verified', async () => {
+    const response = await endpoint.send({
+      name: 'Bob',
+      email: 'bob@example.com',
+      password: 'password123',
+      admin: false,
+    });
+    expect(response.status).toBe(401);
+  });
+
+  test('200 if successful', async () => {
+    const response = await endpoint.send({
+      name: 'Bob',
+      email: 'bob@example.com',
+      password: 'password123',
+      admin: false,
+    });
+    expect(response.status).toBe(200);
+  });
+
+})
+
 describe('POST /auth/register', () => {
   let endpoint: Test;
   beforeEach(() => {
     endpoint = request(server).post('/auth/register');
   });
+
+  // TODO
+  // rejects if no name provided
+  // rejects if email not valid
+  // rejects if user already exists
+  // rejects if password not long enough
+  // rejects if password is incorrect
+  // if successul, return 201
 
   test('creates user', async () => {
     const response = await endpoint.send({
@@ -43,6 +117,107 @@ describe('POST /auth/register', () => {
   //   expect(response.body).toHaveProperty('error');
   // });
 })
+
+describe('POST /auth/forgotPW', () => {
+  let endpoint: Test;
+  beforeEach(() => {
+    endpoint = request(server).post('/auth/forgotPW');
+  });
+
+  // TODO
+  // if user doesn't exist, send 200
+  // if user exists, send 200
+
+  test('sends 200 if user not in db', async () => {
+    const response = await endpoint.send({
+      name: 'Bob',
+      email: 'bob@example.com',
+      password: 'password123',
+      passwordRepeat: 'password123',
+    });
+    expect(response.status).toBe(201);
+  });
+
+  test('sends 200 if user in db', async () => {
+    const response = await endpoint.send({
+      name: 'Bob',
+      email: 'bob@example.com',
+      password: 'password123',
+      passwordRepeat: 'password123',
+    });
+    expect(response.status).toBe(200);
+  });
+})
+
+describe('POST /auth/verifyEmailCode', () => {
+  let endpoint: Test;
+  beforeEach(() => {
+    endpoint = request(server).post('/auth/verifyEmailCode');
+  });
+
+  // TODO
+  // if user doesn't exist, send 401
+  // if user exists but code is incorrect, send 401
+  // if password updated successfully, send 200
+  // if token expires before reset, send 401
+
+  test('creates user', async () => {
+    const response = await endpoint.send({
+      name: 'Bob',
+      email: 'bob@example.com',
+      password: 'password123',
+      passwordRepeat: 'password123',
+    });
+    expect(response.status).toBe(201);
+  });
+})
+
+describe('POST /auth/changePW', () => {
+  let endpoint: Test;
+  beforeEach(() => {
+    endpoint = request(server).post('/auth/changePW');
+  });
+
+  // TODO
+  // if password isn't long enough, send 400
+  // if passwords don't match, send 401
+  // if successful, send 200
+
+  test('rejects if password not long enough', async () => {
+    const response = await endpoint.send({
+      name: 'Bob',
+      email: 'bob@example.com',
+      password: 'passw',
+      passwordRepeat: 'password123',
+    });
+    expect(response.status).toBe(400);
+  });
+})
+
+describe('POST /auth/changePWInApp', () => {
+  let endpoint: Test;
+  beforeEach(() => {
+    endpoint = request(server).post('/auth/changePWInApp');
+  });
+
+  // TODO
+  // if passwords is not correct, send 401
+  // if password isn't long enough, send 400
+  // if passwords don't match, send 401
+  // if password has already been used, send 401
+  // if successful, send 204
+
+  test('rejects if password not long enough', async () => {
+    const response = await endpoint.send({
+      name: 'Bob',
+      email: 'bob@example.com',
+      password: 'passw',
+      passwordRepeat: 'password123',
+    });
+    expect(response.status).toBe(201);
+  });
+})
+
 
 afterAll(async () => {
   await db?.connection.close();
