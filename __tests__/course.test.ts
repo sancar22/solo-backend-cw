39f393d171e4a59b864075456c3d45003b1d381f
CHARLEY CARRIERO <carriero.char@gmail.com>
2021-06-21 20:13:34 -0400

    test setup with auth tokens

--- __tests__/course.test.ts
+++ __tests__/course.test.ts
@@ -5,13 +5,20 @@ import bootServer from '../server';
 import bootDB from '../db/db';
 import { seedDb } from '../__seed__/index';
 import {User} from '../models/user';
+import {Course} from '../models/course';
 
 const port = Number(process.env.COURSE_PORT);
 const connectionString = String(process.env.COURSE_TEST_DB_CONN);
 
+interface tokenResponse {
+  token: string;
+};
+
+
 let server: Server;
 let db: Mongoose | undefined;
 let mockUsers: User[];
+let mockCourses: Course[]
 
 beforeAll(async () => {
   db = await bootDB(connectionString);
@@ -19,6 +26,7 @@ beforeAll(async () => {
     await db?.connection.db.dropDatabase();
     const seedData = await seedDb(db);
     mockUsers = seedData.User;
+    mockCourses = seedData.Course;
   }
   server = bootServer(port);
 });
@@ -26,24 +34,44 @@ beforeAll(async () => {
 describe('POST /enroll/free', () => {
   let endpoint: Test;
   let token: string;
+  //let header: authHeader;
 
-  beforeEach( async () => {
+  beforeEach(async () => {
     endpoint = request(server).post('/course/enroll/free');
   });
 
-  test('a test', async () => {
+
+  test('pre-test login', async () => {
     const response = await request(server).post('/auth/login').send({
       email: "charley@test.com",
       password: "password"
     })
-    expect(response).toHaveProperty('token');
-    token = response.token;
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('token');
+    let resBody: tokenResponse = response.body;
+    token = resBody.token;
+  })
+
+  test('should require authentication', async () => {
+    const response = await endpoint.send({
+      course: {
+        _id: mockCourses[1]._id
+      }
+    })
+    expect(response.status).toBe(401);
+  })
+
+  test('enrolls in free course if authenticated', async () => {
+    const response = await endpoint
+      .send({course: {_id: mockCourses[1]._id}})
+      .set('Authorization', 'bearer ' + token);
+
+    expect(response.status).toBe(200);
+    
   })
 
 
 })
-
-
 
 
 
